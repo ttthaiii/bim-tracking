61247bd07c9235352c66975c4b1c925a49c1f6f1
BBB_12BB <pongsada.brite@gmail.com>
2026-01-28 08:46:27 +0700

    20260127_FixAllFeatureAgain

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,14 +1,26 @@
 import type { AppProps } from 'next/app';
 import { GlobalDataProvider } from '@/contexts/GlobalDataContext';
 import { FirestoreCacheProvider } from '@/contexts/FirestoreCacheContext';
+import { AuthProvider } from '@/context/AuthContext';
+import { DashboardProvider } from '@/context/DashboardContext';
+import { CacheProvider } from '@/context/CacheContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <FirestoreCacheProvider>
-      <GlobalDataProvider>
-        <Component {...pageProps} />
-      </GlobalDataProvider>
-    </FirestoreCacheProvider>
+    <AuthProvider>
+      <DashboardProvider>
+        <CacheProvider>
+          {/* Keep legacy providers if they are still needed for some pages, or typically replace them. 
+              Given the error is about CacheProvider, we strictly ensure it is present. 
+              If the project is migrating, we might wrap everything. */}
+          <FirestoreCacheProvider>
+            <GlobalDataProvider>
+              <Component {...pageProps} />
+            </GlobalDataProvider>
+          </FirestoreCacheProvider>
+        </CacheProvider>
+      </DashboardProvider>
+    </AuthProvider>
   );
 }
 
